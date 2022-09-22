a25d152ceedd0f95c1ae87cb4ed2c5106986d02f
Charles <charlesnwoye2@gmail.com>
2022-09-22 15:16:03 +0100

    initial server setup done

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+
+global.appRoot = path.resolve(__dirname);
+global.appName = "Kwiz-API";
+
+// connect db here
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Accept", "application/json");
+  res.header("Access-Control-Allow-Credentials", 'true');
+  next();
+});
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => { res.json("hello world") });
+app.use((req, res, next) => {
+  next(createError(404, "This URL does not exist!"));
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const app = require('./app');
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
