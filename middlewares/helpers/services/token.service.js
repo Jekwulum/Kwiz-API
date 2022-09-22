959b0d0d253bca8ceaca429056d2a12f4a4153d0
Charles <charlesnwoye2@gmail.com>
2022-09-22 17:19:42 +0100

    token validators

--- middlewares/helpers/services/token.service.js
+++ middlewares/helpers/services/token.service.js
@@ -1,16 +1,32 @@
-require('dotenv').config();
+const config = process.env;
 const jwt = require('jsonwebtoken');
 const TokenModel = require('../../../models/token.model');
 const UserModel = require('../../../models/user.model');
 
 const tokenService = {
   generateToken: (userId, email, UsersRef) => {
-    const token = jwt.sign({ userId, email }, process.env.TOKEN_KEY, { expiresIn: "2h" });
+    const token = jwt.sign({ userId, email }, config.TOKEN_KEY, { expiresIn: "2h" });
     TokenModel.create({ token, userId, UsersRef }, (err, doc) => {
       if (err) res.status(400).json({ status: "FAILED", message: err });
     });
 
     return token;
+  },
+
+  verifyToken: (req, res, next) => {
+    if (req.headers && req.header['authorization']) {
+      const token = req.body.token || req.query.token || req.headers['Authorization'];
+
+      try {
+        req.user = jwt.verify(token, config.TOKEN_KEY);
+        console.log("req.user: ", req.user);
+      } catch (error) {
+        res.status(401).json({ status: "FAILED", message: "Invalid Token" });
+      }
+
+    } else res.status(404).json({ status: "FAILED", message: "Error occurred, validation token not found" });
+
+    return next();
   }
 };
 
